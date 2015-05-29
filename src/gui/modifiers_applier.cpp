de47cc02665f1730bb7fb257081887d43a4ec650
Andrey Matveyakin <a.matveyakin@gmail.com>
2015-05-30 00:15:36 +0200

    Catch exceptions from modifier appliers.

--- src/gui/modifiers_applier.cpp
+++ src/gui/modifiers_applier.cpp
@@ -1,5 +1,6 @@
 #include "drawing_view.h"
 #include "modifiers_applier.h"
+#include "utils/gui_error_handling.h"
 
 #include "colors/simple_color_transforms.h"
 #include "drawing/drawing.h"
@@ -16,13 +17,17 @@ ModifiersApplier::~ModifiersApplier()
 }
 
 void ModifiersApplier::invertColors()
+TRY
 {
     auto filter = std::make_unique< ColorMapFilter< InverseColorTransform > >( InverseColorTransform() );
     m_drawing->addModifier( std::move( filter ) );
 }
+CATCH_SHOW_ERROR
 
 // void ModifiersApplier::invertLightness()
+// TRY
 // {
 //     auto filter = std::make_unique< ColorMapFilter< InverseLightnessColorTransform > >( InverseLightnessColorTransform() );
 //     m_drawing->addModifier( std::move( filter ) );
 // }
+// CATCH_SHOW_ERROR
